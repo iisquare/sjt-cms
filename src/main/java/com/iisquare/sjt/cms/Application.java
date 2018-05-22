b82f42440e5e57857d072ee93dea55c3388c7a30
iisquare <iisquare@163.com>
2018-05-22 13:06:43 +0800

    elementui

--- src/main/java/com/iisquare/sjt/cms/Application.java
+++ src/main/java/com/iisquare/sjt/cms/Application.java
@@ -1,13 +1,16 @@
 package com.iisquare.sjt.cms;
 
+import com.iisquare.sjt.cms.core.BeanNameGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableJpaRepositories(basePackages = {"com.iisquare.sjt.cms.dao"})
+@ComponentScan(nameGenerator = BeanNameGenerator.class)
 public class Application {
 
     public static void main(String[] args) {
