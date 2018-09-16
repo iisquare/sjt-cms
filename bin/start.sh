b6528a73c94ffc27a1571fa46ba672a9cc20c016
IISquare <iisquare@163.com>
2018-09-16 20:00:05 +0800

    shell

--- bin/start.sh
+++ bin/start.sh
@@ -2,6 +2,6 @@
 source $(dirname $0)/env.sh
 for pjt in $PROJECT_APP
 do
-java -jar ${PROJECT_ROOT}/${pjt}/build/libs/sjt-${pjt}.jar
+$JAVA_HOME/bin/java -jar ${PROJECT_ROOT}/${pjt}/build/libs/sjt-${pjt}.jar
 done
 
