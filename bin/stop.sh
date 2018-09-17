6e3600f65770565d201ce2908ad1fb9dca28259c
iisquare <iisquare@163.com>
2018-09-17 09:33:17 +0800

    shell

--- bin/stop.sh
+++ bin/stop.sh
@@ -2,6 +2,6 @@
 source $(dirname $0)/env.sh
 for pjt in ${PROJECT_APP[@]}
 do
-ps aux|grep 'sjt-${pjt}.jar'|awk '{print $2}'|xargs kill -s TERM
+ps aux|grep "sjt-${pjt}.jar"|awk '{print $2}'|xargs kill -s TERM
 done
 
